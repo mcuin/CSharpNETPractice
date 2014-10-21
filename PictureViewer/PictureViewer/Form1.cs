3231c06a76fffbcd922f074bd62a3570292bbb63
Mykal Cuin <mykcuin@gmail.com>
2014-10-21 11:56:21 -0600

    Added more code to the buttons. Finished with Microsoft tutorial one.

--- PictureViewer/PictureViewer/Form1.cs
+++ PictureViewer/PictureViewer/Form1.cs
@@ -24,30 +24,42 @@ namespace PictureViewer
 
         private void closeButton_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
 
         private void showButton_Click(object sender, EventArgs e)
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
+                //Shows the open file dialog box, and will open chosen picture. 
                 pictureBox1.Load(openFileDialog1.FileName);
             }
         }
 
         private void clearButton_Click(object sender, EventArgs e)
         {
-
+            //Sets the image to be blank
+            pictureBox1.Image = null;
         }
 
         private void backgroundButton_Click(object sender, EventArgs e)
         {
-
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.BackColor = colorDialog1.Color;
+            }
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (checkBox1.Checked)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+            }
         }
     }
 }
