cf532c5dbb43f3605704cf06306b4ab1e6263d01
Mykal Cuin <mykcuin@gmail.com>
2014-10-23 13:38:51 -0600

    Added the math for multiply and divide.

--- MathQuiz/MathQuiz/Form1.cs
+++ MathQuiz/MathQuiz/Form1.cs
@@ -44,6 +44,13 @@ namespace MathQuiz
             product.Value = 0;
 
             div1 = randomizer.Next(2, 11);
+            int temp = randomizer.Next(2, 11);
+            div2 = div1 * temp;
+
+            divLabelLeft.Text = div2.ToString();
+            divLabelRight.Text = div1.ToString();
+
+            quotent.Value = 0;
 
             timeLeft = 30;
             timeLabel.Text = "30 seconds";
@@ -86,13 +93,15 @@ namespace MathQuiz
                 MessageBox.Show("You didn't finish in time, sorry!");
                 sum.Value = addEnd1 + addEnd2;
                 subtract.Value = sub1 - sub2;
+                product.Value = mult1 * mult2;
+                quotent.Value = div2 / div1;
                 startButton.Enabled = true;
             }
         }
 
         private bool CheckAnswer()
         {
-            if ((addEnd1 + addEnd2 == sum.Value) && (sub1 - sub2 == sum.Value))
+            if ((addEnd1 + addEnd2 == sum.Value) && (sub1 - sub2 == subtract.Value) && (mult1 * mult2 == product.Value) && (div2 / div1 == quotent.Value))
             {
                 return true;
             }
