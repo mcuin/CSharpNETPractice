b0c874a4d64af04c55f25e01067dce089a18ec21
Mykal Cuin <mykcuin@gmail.com>
2014-10-23 14:28:22 -0600

    Added a list of strings that will be used to match.

--- MatchingGame/MatchingGame/Form1.cs
+++ MatchingGame/MatchingGame/Form1.cs
@@ -12,6 +12,13 @@ namespace MatchingGame
 {
     public partial class Form1 : Form
     {
+
+        Random random = new Random();
+
+        List<string> icons = new List<string>(){
+            "!", "!", "N", "N", ",", ",", "k", "k", "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
         public Form1()
         {
             InitializeComponent();
