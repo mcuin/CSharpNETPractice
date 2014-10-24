4d2ce88ef3a5dd4f24f105adddf94cc612b7b548
Mykal Cuin <mykcuin@gmail.com>
2014-10-24 11:33:46 -0600

    Created a basic console project that does hello world and command line arguments.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("You entered the following {0} command line arguments:", args.Length);
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("{0}", args[i]);
+            }
+
+            return 0;
+        }
+    }
+}
